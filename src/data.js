30807b087a753c2031c9c9dd3b53bfdc74a290c0
Mirisaurio <Mirisaurio>
2023-06-27 12:12:15 -0600

    elemento dialog se muestra correctamente

--- src/data.js
+++ src/data.js
@@ -8,3 +8,17 @@ export function obtenerData() {
   return data.results
 };
 
+ export function filtrarporStatus( status, arregloDePersonajes) 
+{
+  // averiguar que es status, ya sabemos que status es un string que puede tener 2 valores:
+  // "Alive" o "dead"
+  // si estatus === "Alive" -> 
+  // vamos a recorrer nuestro arreglo de personajes preguntando si ese personaje esta vivo
+  // es decir su status tiene la propiedad "Alive"
+  // si si cumple esta condicion agregamos a ese personaje a nuestro arregl oresultado 
+  // al terminar de recorrer el arreglo devolvemos arreglo resultado 
+
+
+  const arregloResultado = [];
+  return arregloResultado;
+}
--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ import { obtenerData } from './data.js';
 const resultado= obtenerData();
 //console.log(resultado);
 const primerContenedor = document.getElementById("contenedorPadre")
+const ventanaModal = document.getElementById("modal")
 
 //console.log(listadePersonajes);
 function mostrarPersonajes(arregloDePersonajes){
-    for (let i = 0; i < arregloDePersonajes.length; i++) {
+    for (let i = 0; i < arregloDePersonajes.length; i=i+1) {
         const personajes = arregloDePersonajes[i];
        //console.log(personajes.image,personajes.name);
       // console.log(personajes.name);
       
-
+     //ingreso de tarjeta con todos los personajes
       const segundoContenedor = document.createElement("div")
       segundoContenedor.classList.add("estiloSegundo")
       const imagenPersonaje=document.createElement("img")
@@ -25,18 +26,76 @@ function mostrarPersonajes(arregloDePersonajes){
       segundoContenedor.appendChild(imagenPersonaje)
       segundoContenedor.appendChild(nombrePersonajes)
       primerContenedor.appendChild(segundoContenedor)
-     
+       
+      //funcion para modal
      segundoContenedor.addEventListener("click",()=>{
-        console.log(personajes.name);
+        //console.log(personajes.image,personajes.name);
+      ventanaModal.innerHTML= ""
+        //creación de div de modal con los datos especificos de cada personaje
+        const ContenedorModal=document.createElement("div")
+        ContenedorModal.classList.add("contenedorModal")
+
+        const nombrePersonajesModal=document.createElement("h3")
+        nombrePersonajesModal.classList.add("nombreModal")
+        nombrePersonajesModal.textContent = personajes.name;
+        
+        const imagenPersonajeModal=document.createElement("img")
+        imagenPersonajeModal.classList.add("imagenModal")
+        imagenPersonajeModal.src=personajes.image;
+
+        
+        const ContenedorDetalle=document.createElement("div")
+        ContenedorDetalle.classList.add("detalle")
+
+        const ContenedorLista=document.createElement("div")
+        ContenedorLista.classList.add("contenedordeLista")
+        
+
+        const statusPersonaje=document.createElement("li")
+        statusPersonaje.classList.add("lista")
+        statusPersonaje.textContent="Status:" + " " + personajes.status;
+        
+        ContenedorLista.appendChild(statusPersonaje)
+
+        const speciesPersonaje=document.createElement("li")
+        speciesPersonaje.classList.add("lista")
+        speciesPersonaje.textContent="Specie:"+ " " + personajes.species;
+
+        ContenedorLista.appendChild(speciesPersonaje)
+
+        const generoPersonaje=document.createElement("li")
+        generoPersonaje.classList.add("lista")
+        generoPersonaje.textContent="Gender:"+ " "+ personajes.gender;
+
+        ContenedorLista.appendChild(generoPersonaje)
+
+        const origenPersonaje=document.createElement("li")
+        origenPersonaje.classList.add("lista")
+        origenPersonaje.textContent="Origin:" + " " + personajes.origin.name;
+
+        ContenedorLista.appendChild(origenPersonaje)
+
+        const locacionPersonaje=document.createElement("li")
+        locacionPersonaje.classList.add("lista")
+        locacionPersonaje.textContent= "Location:" + " " + personajes.location.name;
+
+        ContenedorLista.appendChild(locacionPersonaje)
+
+
+        ContenedorModal.appendChild(nombrePersonajesModal)
+        ContenedorDetalle.appendChild(imagenPersonajeModal)
+        ContenedorDetalle.appendChild(ContenedorLista)
+        ContenedorModal.appendChild(ContenedorDetalle)
+
+        ventanaModal.appendChild(ContenedorModal)
+        ventanaModal.showModal();
+
      })
+
     }
 }
 mostrarPersonajes(resultado);
 
-function pantallaEmergente(informacion){
 
-
-}
-pantallaEmergente(personajes);
 
 //console.log(resultado);
